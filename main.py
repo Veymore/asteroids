e66bfecf2abf3ca56fdcb1ed994939faa90ab07f
Veymore <95536830+Veymore@users.noreply.github.com>
2024-11-06 13:07:46 +0100

    use less resources by introducing game clock and add CircleShape base class

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def main() -> None:
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    game_clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt += game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
